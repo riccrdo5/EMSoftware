6f19a47d1b1f624115d35fd0e3fd60dc2ff53830
Manjunath Bhat <mbhat2@buffalo.edu>
2020-10-12 14:07:31 -0400

    Added venv and venmo button

--- EssentialMachine/__init__.py
+++ EssentialMachine/__init__.py
@@ -2,19 +2,26 @@ from flask import Flask, url_for, render_template
 from .database_handler import DatabaseHandler
 
 app = Flask(__name__)
-#app.config.from_object('config.Config')
+# app.config.from_object('config.Config')
 
-#DB_NAME = app.config["DATABASE_NAME"]
-#DB_SEED_FILE = app.config["DATABASE_SEED_FILE"]
+# DB_NAME = app.config["DATABASE_NAME"]
+# DB_SEED_FILE = app.config["DATABASE_SEED_FILE"]
 
 DB_NAME = "essential_machine.db"
 DB_SEED_FILE = "./seed.json"
 
+
 @app.route('/')
 def hello(name=None):
-	return render_template('index.html', name=name)
+    return render_template('index.html', name=name)
+
+
+@app.route('/billing_summary')
+def billing_summary(name=None):
+    return render_template('billing_summary', name=name)
+
 
 if __name__ == "__main__":
-	db_handler = DatabaseHandler(DB_NAME, DB_SEED_FILE)
-	db_handler.seed_database()
-	app.run()
+    db_handler = DatabaseHandler(DB_NAME, DB_SEED_FILE)
+    db_handler.seed_database()
+    app.run()
